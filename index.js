87f1e2b8a166e6c30353658830c38844e1852a86
Babak Doraniarab <babak.dorani@gmail.com>
2022-06-09 13:53:56 +0300

    dockerignore is added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/',(req, res) => {
-    console.log('req ');
-})
+app.get("/", (req, res) => {
+  req.send("<h2>Hi test</h2>");
+});
 app.listen(port, () => console.log(`we are listening on port ${port}`));
